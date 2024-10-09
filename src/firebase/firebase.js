ba567b009f429197ec0b110650d4d286d3b0363a
dengolden <antoniomarquez1593@gmail.com>
2024-10-09 17:03:56 +0700

    fixing initialize db

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,5 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getFirestore } from 'firebase/firestore';
 import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -16,6 +17,14 @@ const firebaseConfig = {
   measurementId: "G-Y0CHKDMP1M"
 };
 
+
+
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+const db = getFirestore(app);
+
+export { db };
+// const analytics = getAnalytics(app);
+// if (typeof window !== 'undefined') {
+//   analytics = getAnalytics(app); // Analytics will only be initialized in client-side
+// }
