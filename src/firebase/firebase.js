bcbe62b682d4b532741eb4f99327cdc709bfe78d
Deo Lolowang <justinlolowang9@gmail.com>
2024-10-10 13:22:57 +0700

    update initialize firebase

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,30 +1,25 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
+import { initializeApp, getApps } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBauzeTL2K73CdnxKKLc_vNA60eY6qFxSY",
-  authDomain: "procurement-app-d4fdc.firebaseapp.com",
-  projectId: "procurement-app-d4fdc",
-  storageBucket: "procurement-app-d4fdc.appspot.com",
-  messagingSenderId: "599120600800",
-  appId: "1:599120600800:web:351bea4cb5f4290588fa85",
-  measurementId: "G-Y0CHKDMP1M"
+  apiKey: process.env.NEXT_PUBLIC_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_APP_ID,
 };
 
+// Inisialisasi
+let app;
+if (!getApps().length) {
+  app = initializeApp(firebaseConfig);
+}
 
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
+// Inisialisasi Auth dan Firestore
+const FirebaseAuth = getAuth(app);
 const db = getFirestore(app);
 
-export { db };
-// const analytics = getAnalytics(app);
-// if (typeof window !== 'undefined') {
-//   analytics = getAnalytics(app); // Analytics will only be initialized in client-side
-// }
+export { FirebaseAuth, db };
