420bc574fd5c29f99954c084dc25a6db7fdbf9d5
benedictjefferson01 <benedictjefferson01@adakami.id>
2024-11-06 10:38:04 +0700

    update metadata

--- src/constants/metadata.tsx
+++ src/constants/metadata.tsx
@@ -1,11 +1,5 @@
-/**
- * Author: Barkah Hadi
- * Description: This file is used to store metadata of the app
- * Last Modified: 01-05-2023
- */
-
 const metadata = {
-  APP_NAME: "NextJS Boilerplate with antd",
+  APP_NAME: "Procurement Web App",
   APP_DESCRIPTION:
     "NextJS Boilerplate is a boilerplate for NextJS with antd and Redux",
   KEYWORDS: "nextjs, antd, redux, boilerplate",
