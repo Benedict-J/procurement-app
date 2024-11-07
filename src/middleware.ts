f153ad2c5bbf2a9d1f215008a10ee7849ec75805
benedictjefferson01 <benedictjefferson01@adakami.id>
2024-11-07 09:21:19 +0700

    feat: middleware

---
+++ src/middleware.ts
@@ -0,0 +1,36 @@
+import { auth } from '@/firebase/firebase';
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
+
+export const getUserToken = async () => {
+  const user = auth?.currentUser;
+
+  if (user) {
+    try {
+      const idToken = await user.getIdToken();
+      console.log("ID Token:", idToken);
+      return idToken;
+    } catch (error) {
+      console.error("Error fetching ID token:", error);
+      throw error;
+    }
+  } else {
+    console.log("No user is logged in.");
+    return null;
+  }
+};
+
+export async function middleware(request: NextRequest) {
+  const token = await getUserToken();
+  const urlObj = new URL(request.url);
+  
+  if (!token) {
+    return NextResponse.redirect(new URL('/auth/login', urlObj.origin));
+  }
+
+  return NextResponse.next()
+}
+ 
+export const config = {
+  matcher: ["/((?!auth/*|_next/static|next/image).*)"],
+};
